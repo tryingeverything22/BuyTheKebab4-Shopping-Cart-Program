4dc865b477350c54150341f40ab4e404b7bfc80e
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-26 22:34:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Exercise 2 Shopping Cart Program ( BUY THE KEBAB 12.99 SALE) :3
+
+item = input ("what item would you like to buy?:")
+price = float(input ("What is the price:"))
+quantity = int(input ("How many would you like?:"))
+total = price * quantity
+
+print (total)
+
+
